368b7fd914c53430c713ca7082093856dabb8138
Thorsten Sick <thorsten.sick@email.de>
2020-06-04 13:38:32 +0200

    Added first unit tests

---
+++ test/test_demo.py
@@ -0,0 +1,26 @@
+
+# Just a unit test demo to test the github integration
+# Copied from the example
+#
+#
+
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['helo', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
